81edd130ec0bba63cf18ebdc63989da787044c7b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-04-14 11:29:29 +0200

    a

--- include/phany/phany.hpp
+++ include/phany/phany.hpp
@@ -16,63 +16,75 @@ struct test
     
     ~test ()
     {
-        cout << i << endl;
+//        cout << i << endl;
         --tot;
     }
 };
 
-struct phany
+struct _phany
 {
     
-    using arena_type = arena <50000000>;
+    
+    using arena_type = arena <500000>;
     
     inline static arena_type m_arena;
 //    template <typename T>
 //    explicit phany (phany&) = delete;
     
     
-    template <typename T>
-    requires (not is_same_v <T, phany&>)
-    phany (T const& value) : m_value {m_arena}, m_destruct {[] (byte* value) {reinterpret_cast <T*> (value)->~T ();}}, m_allocate {[] (arena_type& a) {return (T*) a;}}, m_construct {[] (byte* dst) {}}
-    {
-        new (m_value) T {value};
-    }
-    
-  
+//    template <typename T>
+//    requires (not is_same_v <T, phany&>)
+//    phany (T const& value) : m_value {m_arena}, m_destruct {[] (byte* value) {reinterpret_cast <T*> (value)->~T ();}}//, m_allocate {[] (arena_type& a) {return (T*) a;}}//, m_construct {[] (byte* dst) {}}
+//    {
+//        new (m_value) T {value};
+//    }
     
     template <typename T>
-    requires (not is_same_v <T, phany&>)
-    phany (T&& value) : m_value {m_arena}, m_destruct {[] (byte* value) {reinterpret_cast <T*> (value)->~T ();}}
+    requires (not is_class_v <T>)
+    _phany (T&& value) : m_value {m_arena}
     {
         new (m_value) T {move (value)};
     }
     
+    template <typename T>
+    requires (not is_same_v <T, _phany&> and is_class_v <T>)
+    _phany (T&& value) :
+        m_value {m_arena},
+        m_destruct {[](byte*& value){
+            reinterpret_cast <T*> (value)->~T ();
+        }}
+    {
+        new (m_value) T {forward <T> (value)};
+    }
+    
+  
+    
    
-    phany (phany&& other) : phany ()
+    _phany (_phany&& other) : _phany ()
     {
         swap (*this, other);
     }
     
-    phany (phany const& other)
-    {
-    }
+//    phany (phany const& other)
+//    {
+//    }
     
-    auto operator= (phany const& other) -> phany&
-    {
-        return *this;
-    }
+//    auto operator= (phany const& other) -> phany&
+//    {
+//        return *this;
+//    }
     
-    auto operator= (phany other) -> phany&
-    {
-        swap (*this, other);
-        return *this;
-    }
+//    auto operator= (phany other) -> phany&
+//    {
+//        swap (*this, other);
+//        return *this;
+//    }
     
     
-    friend auto swap (phany& lhs, phany& rhs) -> void
+    friend auto swap (_phany& lhs, _phany& rhs) -> void
     {
         using std::swap;
-        
+
         swap (lhs.m_value, rhs.m_value);
         swap (lhs.m_destruct, rhs.m_destruct);
     }
@@ -83,10 +95,13 @@ struct phany
 //    template <typename T>
     
     
-    phany () = default;
+    _phany () : m_destruct {[](byte*&){}}
+    {
+        
+    }
   
     
-    ~phany ()
+    ~_phany ()
     {
         m_destruct (m_value);
         m_arena (m_value);
@@ -94,19 +109,30 @@ struct phany
     
 private:
     byte* m_value {nullptr};
-    void (*m_destruct) (byte*) = []<typename T>(T*){};
-    byte* (*m_allocate) (arena_type&) = [](arena_type&) -> byte* {return nullptr;};
-    void (*m_construct) (byte*) = [](byte*){};
-    void (*m_construact) (byte*) = [](byte*){};
-    void (*m_construkact) (byte*) = [](byte*){auto a = []{};auto b = []{}; auto c = []{};};
+    void (*m_destruct) (byte*&);
+//    function <void (byte*)> m_destruct;
+    // = [](byte*){};
+//    byte* (*m_allocate) (arena_type&) = [](arena_type&) -> byte* {return nullptr;};
+//    void (*m_construct) (byte*) = [](byte*){};
+//    void (*m_construact) (byte*) = [](byte*){};
+//    void (*m_construkact) (byte*) = [](byte*){auto a = []{};auto b = []{}; auto c = []{};};
+//    inline static constexpr void (*m_construlkkact) (int i) = []<typename T>(auto i) constexpr {if constexpr(is_same_v <int, T>){}auto a = []{};auto b = []{}; auto c = []{};};
 
     
-    
+//    decltype ([]<typename T>(auto i) constexpr {if constexpr(is_same_v <int, T>){}auto a = []{};auto b = []{}; auto c = []{};}) a;
     
 };
 
 
 
+template <typename T>
+struct phany
+{
+    T t;
+};
+
+template <typename T>
+phany (T) -> phany <decay_t <T>>;
 
 
 
--- tests/test_phany.cpp
+++ tests/test_phany.cpp
@@ -1,5 +1,6 @@
 #include <catch2/catch.hpp>
 #include <phany/phany.hpp>
+#include <ph_time/timer.hpp>
 using namespace std;
 
 TEST_CASE ("")
@@ -7,12 +8,53 @@ TEST_CASE ("")
     any a1;
 //    phany p1;
     phany p2 {test {}};
-    phany p3 = p2;
-    cout << sizeof (phany) << endl;
+    phany p3 = move (p2);
+//    cout << sizeof (phany) << endl;
     cout << sizeof (any) << endl;
+    
 //    phany p4 = p3;
 //    test a {};
     
     cout << "=========" << endl;
 }
 
+TEST_CASE("is class")
+{
+    int max = 10000000;
+    {
+        Timer<true> t ("any");
+        for (int i = 0; i < max; ++i)
+        {
+            any a1 {test {}};
+        }
+    }
+    {
+        Timer<true> t ("phany");
+        for (int i = 0; i < max; ++i)
+        {
+            phany a1 {test {}};
+        }
+    }
+    
+    
+}
+
+TEST_CASE("is not class")
+{
+    return;
+    int max = 100000000;
+    {
+        Timer<true> t ("any");
+        for (int i = 0; i < max; ++i)
+        {
+            any a1 {2};
+        }
+    }
+    {
+        Timer<true> t ("phany");
+        for (int i = 0; i < max; ++i)
+        {
+            phany a1 {2};
+        }
+    }
+}
